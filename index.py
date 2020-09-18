7daca26bc35da6a091c142e058e3f75dce17f591
William Myers <williamrmyers@gmail.com>
2020-09-18 14:30:51 -0700

    Added state of the stock, by storing JSON in file. You will be updated on of additions to stock. You will always be notified on first run.

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 # Python program to scrape website
 # and save quotes from website
 import os
+from os import path
 import requests
 from bs4 import BeautifulSoup
 from twilio.rest import Client
+import json
 
 # Get env Variables
 TWILIO_ACCOUNT_ID = os.environ.get('TWILIO_ACCOUNT_ID')
@@ -25,8 +27,36 @@ for row in soup.findAll('a',
     if (row.getText() == "Add to cart"):
         urls.append("https://gardinonursery.com/" + row['href'])
 
+
+tempfilename = "prev-stock.json"
+# Check if temp file exists
+if not path.exists(tempfilename):
+    print('Creating File:', tempfilename)
+    # Creates File
+    f = open(tempfilename, "x")
+    # The first time it runs, you will always have new stock.
+    new_stock = True
+
+else:
+    # Read in file
+    with open(tempfilename, "r") as read_file:
+        prev_stock = json.load(read_file)
+        # Compare new list to old
+        # Set bool to indicate that number of avalable flowers has changed.
+        if(len(urls) > len(prev_stock)):
+            print('New Stock')
+            new_stock = True
+        else:
+            print('no new Stock')
+            new_stock = False
+
+# Write to file with new stock list
+with open(tempfilename, "w") as write_file:
+    json.dump(urls, write_file)
+
 # Seperate Urls into comma seperated Values
 urlsString = '\n\n'.join(map(str, urls))
+
 
 body = f'{len(urls)} Hoyas are avalable! Thay\'re at these URLs {urlsString}'
 
@@ -47,8 +77,9 @@ def sendText():
     print(message.sid)
 
 
-# Notify user of count is above 1
-if (len(urls) > 1):
+# Notify user of count is above 1 & if the number of urls is greater than earlier run
+# First condition is counting the number of non flower items like pots
+if (new_stock == True):
     sendText()
 else:
     print('No Hoyas avalable')
